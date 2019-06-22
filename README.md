c9d51c765e0df37758923aef76b6bad2527f2bd1
furimako <furimako@gmail.com>
2019-06-22 19:54:34 +0900

    update the extension

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # open-news-today
 Google Chrome Extension for opening today's news.
+
+## How to Use
+### Nikkei News
+1. Go to [Nikkei HP](https://www.nikkei.com/)
+1. Open this Extension menu and press [Open Nikkei News] button.
+
+### Hacker News
+1. Go to [Hacker News HP](https://news.ycombinator.com/)
+1. Open this Extension menu and press [Open Hacker News] button.
--- popup.js
+++ popup.js
@@ -1,21 +1,15 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+const nikkei = document.getElementById('nikkei')
+const hackernews = document.getElementById('hackernews')
 
-'use strict';
+function onclickEvent(element) {
+    const selector = element.target.value
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.executeScript(
+            tabs[0].id,
+            { code: `document.querySelectorAll('${selector}').forEach(v => window.open(v.href));` }
+        )
+    })
+}
 
-let changeColor = document.getElementById('changeColor');
-
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
-
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
-  });
-};
+nikkei.onclick = onclickEvent
+hackernews.onclick = onclickEvent
