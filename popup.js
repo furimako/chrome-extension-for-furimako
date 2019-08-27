d047f2bfeb2961c516be96e7c1e6b42a839e542b
furimako <furimako@gmail.com>
2019-08-27 11:49:22 +0900

    modify open logic for hacker News

--- popup.js
+++ popup.js
@@ -1,5 +1,11 @@
 /* global chrome */
-
+const queries = {
+    nikkei: 'document.querySelectorAll(\'#JSID_baseRefreshNxTop2 > div.m-miM09 > h3 > a\').forEach(v => window.open(v.href));'
+        + 'document.querySelectorAll(\'#JSID_baseRefreshNxTop2 > div.m-miM10 > div > h3 > a\').forEach(v => window.open(v.href));',
+    hackerNews: 'document.querySelectorAll(\'table.itemlist > tbody > tr.athing > td:nth-child(3) > a\')'
+        + '.forEach((v, i) => { if (i < 10) window.open(v.href) });',
+    google: 'document.querySelectorAll(\'div.srg > div.g > div > div > div:nth-child(1) > a\').forEach(v => window.open(v.href));'
+}
 const openButton = document.getElementById('openButton')
 
 openButton.onclick = () => {
@@ -7,12 +13,11 @@ openButton.onclick = () => {
         let code
         
         if (tabs[0].url === 'https://www.nikkei.com/') {
-            code = 'document.querySelectorAll(\'#JSID_baseRefreshNxTop2 > div.m-miM09 > h3 > a\').forEach(v => window.open(v.href));'
-                + 'document.querySelectorAll(\'#JSID_baseRefreshNxTop2 > div.m-miM10 > div > h3 > a\').forEach(v => window.open(v.href));'
+            code = queries.nikkei
         } else if (tabs[0].url === 'https://news.ycombinator.com/') {
-            code = 'document.querySelectorAll(\'table.itemlist > tbody > tr.athing > td:nth-child(3) > a\').forEach(v => window.open(v.href));'
+            code = queries.hackerNews
         } else if (tabs[0].url.includes('https://www.google.com/search')) {
-            code = 'document.querySelectorAll(\'div.srg > div.g > div > div > div:nth-child(1) > a\').forEach(v => window.open(v.href));'
+            code = queries.google
         } else {
             console.log(`url: ${tabs[0].url}`)
             return
