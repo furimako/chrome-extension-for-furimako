ddc662749873171deac7f79dbc2c9d4600c8379e
furimako <furimako@gmail.com>
2019-10-04 18:54:22 +0900

    update query for Nikkei & delete function for Google Search

--- README.md
+++ README.md
@@ -2,8 +2,7 @@
 Google Chrome Extension for opening ALL links at once.
 
 ## How to Use
-Enter [Ctrl + Shift + A] on below sites.
+Enter [Ctrl (Command) + Shift + A] on below sites.
 
 - [Nikkei](https://www.nikkei.com/)
 - [Hacker News](https://news.ycombinator.com/)
-- [Google Search](https://www.google.com/)
--- background.js
+++ background.js
@@ -1,28 +1,27 @@
 /* global chrome */
 
 const queries = {
-    nikkei: 'document.querySelectorAll(\'#JSID_baseRefreshNxTop2 > div.m-miM09 > h3 > a\').forEach(v => window.open(v.href));'
-        + 'document.querySelectorAll(\'#JSID_baseRefreshNxTop2 > div.m-miM10 > div > h3 > a\').forEach(v => window.open(v.href));',
+    nikkei: 'document.querySelectorAll(\'body > main > div.k-hub-body > div.k-layout__container > div > div > div > div > div.k-hub-card.k-hub-card--no-ellipsis > div > a\').forEach(v => window.open(v.href));',
     hackerNews: 'document.querySelectorAll(\'table.itemlist > tbody > tr.athing > td:nth-child(3) > a\')'
-        + '.forEach((v, i) => { if (i < 10) window.open(v.href) });',
-    google: 'document.querySelectorAll(\'#rso > div.bkWMgd:nth-child(1) > div.srg > div.g > div > div.rc > div.r:nth-child(1) > a\').forEach(v => window.open(v.href));'
+        + '.forEach((v, i) => { if (i < 10) window.open(v.href) });'
 }
 
 chrome.commands.onCommand.addListener((command) => {
     console.log(`Command: ${command}`)
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        console.log(`url: ${tabs[0].url}`)
+        
         let code
-        if (tabs[0].url === 'https://www.nikkei.com/') {
+        if (tabs[0].url.includes('https://www.nikkei.com/')) {
             code = queries.nikkei
-        } else if (tabs[0].url === 'https://news.ycombinator.com/') {
+        } else if (tabs[0].url.includes('https://news.ycombinator.com/')) {
             code = queries.hackerNews
-        } else if (tabs[0].url.includes('https://www.google.com/search')) {
-            code = queries.google
         } else {
-            console.log(`url: ${tabs[0].url}`)
+            console.log('match no URLs')
             return
         }
         
+        console.log(`code: ${code}`)
         chrome.tabs.executeScript(tabs[0].id, { code })
     })
 })
